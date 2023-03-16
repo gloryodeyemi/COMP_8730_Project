9592d784df320b69670120ee8762e87b1349d514
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-03-16 03:38:34 -0400

    data analysis updated

--- utils/analysis.py
+++ utils/analysis.py
@@ -16,20 +16,40 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
+# function to tokenize the tweets into sentences
+def tokenize_sent(text):
+    sent_tokens = sent_tokenize(text.lower())
+    return sent_tokens
+
 # function to count the number of sentences in the text
 def count_sents(text):
-    sent_count = len(sent_tokenize(text))
+    sent_count = len(tokenize_sent(text))
     return sent_count
-
-# function to count the number of words in the text
-def count_words(text):
-    word_count = len(word_tokenize(text))
-    return word_count
 
 # function to count the number of characters in the text
 def count_chars(text):
     char_count = len(text.replace(" ",""))
     return char_count
+
+# function to count the number of punctuation in the text
+def count_punc(text):
+    punc_count = len([a for a in text if a in string.punctuation])
+    return punc_count
+
+# function to remove punctuations in the text
+def remove_punctuation(text):
+    without_punc = "".join([i for i in text if i not in string.punctuation])
+    return without_punc
+
+# function for tokenizing the text into words
+def tokenize_text(text):
+    tokens = word_tokenize(text.lower())
+    return tokens
+
+# function to count the number of words in the text
+def count_words(text):
+    word_count = len(tokenize_text(text))
+    return word_count
 
 # function to calculate the word density in the text
 def word_density(data, colname1, colname2):
@@ -41,32 +61,17 @@ def sent_density(data, colname1, colname2):
     s_density = data[colname1] / (data[colname2] + 1)
     return s_density
 
-# function to count the number of punctuation in the text
-def count_punc(text):
-    punc_count = len([a for a in text if a in string.punctuation])
-    return punc_count
-
-# function to display sum of values in all numeric columns 
+# function to display sum of values in all numeric columns
 def data_sum(data):
     print("Sum of values in numerical columns")
     print("----------------------------------")
     print(data.sum(axis=0, numeric_only = True))
-    
-# function to display average of values in all numeric columns 
+
+# function to display average of values in all numeric columns
 def data_average(data):
     print("Average of values in numerical columns")
     print("--------------------------------------")
     print(data.mean(axis=0, numeric_only = True))
-
-# function to remove punctuations in the text
-def remove_punctuation(text):
-    without_punc = "".join([i for i in text if i not in string.punctuation])
-    return without_punc
-
-# function for tokenizing the text
-def tokenize_text(text):
-    tokens = word_tokenize(text.lower())
-    return tokens
 
 """
     function to get number of english and yoruba words from the code-switch tweets 
@@ -109,4 +114,16 @@ def lang_count_comp(data, col1, col2):
     print("Number of Eng words: ", count_eng)
     print(f"Percentage of Yoruba words: {((count_yor/(count_yor + count_eng)) * 100):.2f}%")
     print(f"Percentage of English words: {((count_eng/(count_yor + count_eng)) * 100):.2f}%")
+
+# function to get number of unique words
+def get_unique_words(data, col, alias):
+    results = set()
+    data[col].apply(results.update)
+    print(f"Number of unique words in {alias} = {len(results)}")
+
+# function to get number of unique sentences
+def get_unique_sents(data, col, alias):
+    results = set()
+    data[col].apply(results.update)
+    print(f"Number of unique sentences in {alias} = {len(results)}")
 
